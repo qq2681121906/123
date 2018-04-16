e50109e5d76166ad8a418d1d2195b75f071d8c06
qq2681121906 <2681121906@qq.com>
2018-04-16 18:51:01 +0800

    复习

---
+++ 16-day/4-随机.py
@@ -0,0 +1,38 @@
+import random
+import time
+list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
+a = random.randint(0,len(list)-1)
+print("------ %s"%list[a])
+b = random.randint(0,len(list)-1)
+print("------%s\n"%list[b])
+c = random.randint(0,len(list)-1)
+print("------%s\n"%list[c])
+d = random.randint(0,len(list)-1)
+print("------%s\n"%list[d])
+e = random.randint(0,len(list)-1)
+print("------%s\n"%list[e])
+f = random.randint(0,len(list)-1)
+print("------%s\n"%list[f])
+g = random.randint(0,len(list)-1)
+print("------%s\n"%list[g])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 16-day/5-随机2.py
@@ -0,0 +1,26 @@
+import random
+import time
+list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+a = random.randint(0,len(list)-1)
+print("------ %s"%list[a])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
